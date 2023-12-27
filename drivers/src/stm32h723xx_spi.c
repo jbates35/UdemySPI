d960052db24567d8618e5912139eae1fc2600be0
Jimmy Bates <jbates.productions@gmail.com>
2023-12-27 14:43:55 -0800

    Untested spi send finished

--- drivers/src/stm32h723xx_spi.c
+++ drivers/src/stm32h723xx_spi.c
@@ -3,6 +3,15 @@
 
 #include <stdio.h>
 
+// Helper functions
+void delay(uint32_t delay_ms) {
+  volatile uint32_t i = 0;
+
+  // TODO: Figure out clock cycles per ms
+  for (i = 0; i < delay_ms * 1000; i++)
+    ;
+}
+
 /**
  * Peripheral clock setup
  * */
@@ -53,11 +62,18 @@ void SPI_init(SPI_Handle_t *p_SPI_handle) {
   uint32_t SPIx_RCC_REG[6] = {RCC->APB2RSTR, RCC->APB1LRSTR, RCC->APB1LRSTR,
                               RCC->APB2RSTR, RCC->APB2RSTR,  RCC->APB4RSTR};
 
+  // Parse through the array of possible SPI peripherals and find the one we are
+  // at
   for (int i = 0; i < sizeof(SPIx_BASE_ADDRS) / sizeof(SPI_RegDef_t *); i++) {
     if (*spi_reg != SPIx_BASE_ADDRS[i])
       continue;
 
+    // Reset the peripheral by turning reset bit on/off
     SPIx_RCC_REG[i] |= (1 << SPIx_EN_BIT_POS[i]);
+
+    // Add a delay to make sure the reset is complete
+    delay(1);
+
     SPIx_RCC_REG[i] &= ~(1 << SPIx_EN_BIT_POS[i]);
 
     break;
@@ -86,12 +102,73 @@ void SPI_init(SPI_Handle_t *p_SPI_handle) {
   (*spi_reg)->CR1 |= (1 << SPI_CR1_IOLOCK) + (1 << SPI_CR1_SPE);
 }
 
-void SPI_deinit(SPI_RegDef_t *p_SPI_x);
+void SPI_deinit(SPI_RegDef_t *p_SPI_x) {
+  // First set IOLOCK bit to modify register
+  p_SPI_x->CR1 &= ~((1 << SPI_CR1_IOLOCK) + (1 << SPI_CR1_SPE));
+
+  // Reset the SPI peripheral before configuring
+  static SPI_RegDef_t *const SPIx_BASE_ADDRS[6] = {SPI1, SPI2, SPI3,
+                                                   SPI4, SPI5, SPI6};
+
+  static uint8_t const SPIx_EN_BIT_POS[6] = {12, 14, 15, 13, 20, 5};
+
+  uint32_t SPIx_RCC_REG[6] = {RCC->APB2RSTR, RCC->APB1LRSTR, RCC->APB1LRSTR,
+                              RCC->APB2RSTR, RCC->APB2RSTR,  RCC->APB4RSTR};
+
+  // Parse through the array of possible SPI peripherals and find the one we are
+  // at
+  for (int i = 0; i < sizeof(SPIx_BASE_ADDRS) / sizeof(SPI_RegDef_t *); i++) {
+    if (p_SPI_x != SPIx_BASE_ADDRS[i])
+      continue;
+
+    // Reset the peripheral by turning reset bit on/off
+    SPIx_RCC_REG[i] |= (1 << SPIx_EN_BIT_POS[i]);
+
+    // Add a delay to make sure the reset is complete
+    delay(1);
+
+    SPIx_RCC_REG[i] &= ~(1 << SPIx_EN_BIT_POS[i]);
+
+    break;
+  }
+
+  // Lock the SPI registers so SPI is ready for use
+  p_SPI_x->CR1 |= (1 << SPI_CR1_IOLOCK);
+}
 
 /**
  * SPI function for sending data
  * */
-void SPI_send(SPI_RegDef_t *p_SPI_x, uint8_t *p_tx_buffer, uint32_t len);
+void SPI_send(SPI_RegDef_t *p_SPI_x, uint8_t *p_tx_buffer, uint32_t len) {
+  while (len > 0) {
+    // Wait until TXE is set
+    while (!(p_SPI_x->SR & SPI_SR_TXP))
+      ;
+
+    int bit_count = (p_SPI_x->CFG1 & (0x1F << SPI_CFG1_DSIZE) - 1) / 8 + 1;
+
+    uint32_t tx_word = 0;
+
+    // Convert dataframe size into the amount of bytes, and therefore the amount
+    // of times len has to be decremented
+    if (bit_count == 1) {
+      tx_word = *((uint8_t *)p_tx_buffer);
+      len--;
+    } else if (bit_count == 2) {
+      tx_word = *((uint16_t *)p_tx_buffer);
+      len -= 2;
+    } else if (bit_count == 3) {
+      tx_word = *((uint32_t *)p_tx_buffer) & 0xFFFFFF;
+      len -= 3;
+    } else if (bit_count == 4) {
+      tx_word = *((uint32_t *)p_tx_buffer);
+      len -= 4;
+    }
+
+    // Send word
+    p_SPI_x->TXDR = tx_word;
+  }
+}
 
 /**
  * SPI function for receiving data
